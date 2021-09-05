5331733c14fe8711f62bcf8d78b9e7f47033a688
Tariq <sydtrq7@gmail.com>
2021-09-05 19:25:38 +0530

    datLayer codes aded

---
+++ codes.js
@@ -0,0 +1,1135 @@
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "QuickQuote",
+    "eventLabel": "How much do you want to borrow ?"
+});
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "QuickQuote",
+    "eventLabel": "What is the purpose of your Loan ?"
+});
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "QuickQuote",
+    "eventLabel": "Tell us about yourself ?"
+});
+
+
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "QuickQuote",
+    "eventLabel": "What is your residency status ?"
+});
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "QuickQuote",
+    "eventLabel": "How can we contact you?"
+});
+
+
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "QuickQuote",
+    "eventLabel": "Please confirm the following"
+});
+
+
+
+
+
+
+
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "RateEstimate",
+    "eventLabel": "Great News"
+});
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "RateEstimate",
+    "eventLabel": "Declined"
+});
+
+
+
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "RateEstimate",
+    "eventLabel": "Rate Estimation Request Submitted",
+    "loan_amount": "<Enter Loan Amount Here>",
+    "repayment_frequency": "<Enter Frequency Here>",
+    "loan_term": "<Enter Loan Term Here>"
+});
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "RateEstimate",
+    "eventLabel": "Profile/Password Setup Request Submitted"
+});
+
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "Application",
+    "eventLabel": "Request Submitted for employment status",
+    "employment_status": "<Enter Employment Status Here>"
+});
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "Application",
+    "eventLabel": "What do you do for living ?",
+    "role": "<Enter Current Role>",
+    "employer_name": "<Enter Employer Name>",
+    "industry": "<Enter Industry Name>"
+});
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "Application",
+    "eventLabel": "Request Submitted for employment duration",
+    "employment_duration": "<Enter Employment Duration here>"
+});
+
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "Application",
+    "eventLabel": "Request Submitted for gross annual income",
+    "income": "<Enter Income Here>"
+});
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "Application",
+    "eventLabel": "Request Submitted for housing status",
+    "housing_status": "<Enter Housing Status>"
+});
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "Application",
+    "eventLabel": "Request Submitted for monthly housing expenses",
+    "housing_expense": "<Enter Housing Expenses>"
+});
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "Application",
+    "eventLabel": "Income Verification",
+    "income_verified": "<Enter Status Here>"
+});
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "Application",
+    "eventLabel": "Application Declined",
+    "income_verified": "<Enter Status Here>"
+});
+
+
+
+
+
+
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "ApplicationOutcome",
+    "eventLabel": "Thank you for your application",
+    "application_status": "<Enter Application Status Here>"
+});
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "ApplicationOutcome",
+    "eventLabel": "ApplicationDeclined",
+    "application_status": "<Enter Application Status Here>"
+});
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "DocumentRequired",
+    "eventLabel": "Document Upload"
+});
+
+
+
+
+
+
+
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "ProductAcceptance",
+    "eventLabel": "Sign in"
+});
+
+
+
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "ProductAcceptance",
+    "eventLabel": "Congratulations"
+});
+
+
+
+
+
+
+
+
+
+
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "bankdetails",
+    "eventLabel": "My Bank Details"
+});
+
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "customer_created",
+    "eventLabel": "Congratulations"
+});
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+    "event": "eventPush",
+    "eventCategory": "formInteraction",
+    "eventAction": "my_profile_click",
+    "eventLabel": ""
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
